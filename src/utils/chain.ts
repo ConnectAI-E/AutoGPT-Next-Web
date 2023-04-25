7383621f11b62e24f987a1cb929dab972a1308f5
NateCC0902 <r7895566@gmail.com>
2023-04-26 01:37:57 +0800

    update some default settings

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -10,10 +10,10 @@ export const createModel = (settings: ModelSettings) =>
       settings.customApiKey === ""
         ? process.env.OPENAI_API_KEY
         : settings.customApiKey,
-    temperature: 0.9,
+    temperature: settings.customTemperature === null ? 0.9 : settings.customTemperature,
     modelName:
       settings.customModelName === "" ? GPT_35_TURBO : settings.customModelName,
-    maxTokens: 750,
+    maxTokens: settings.maxTokens === null ? 750 : settings.maxTokens,
   });
 
 const startGoalPrompt = new PromptTemplate({
