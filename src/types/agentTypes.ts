fec61a45b46ef75624291060ca7c9eeae962c295
lloydzhou <lloydzhou@qq.com>
2023-04-28 21:36:06 +0800

    feat: add taskId to sort result in chat window

--- src/types/agentTypes.ts
+++ src/types/agentTypes.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const messageParser = z.object({
+  taskId: z.string().optional(),
+  parentTaskId: z.string().optional(),
   type: z.enum(["goal", "thinking", "task", "action", "system"]),
   info: z.string().optional(),
   value: z.string(),
