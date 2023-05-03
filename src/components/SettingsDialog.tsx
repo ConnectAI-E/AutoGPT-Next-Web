253d655e67baf0c16d9480bc64a7d388e44da6b4
Dogtiti <499960698@qq.com>
2023-05-03 23:04:07 +0800

    fix: key validate

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -50,7 +50,7 @@ export const SettingsDialog: React.FC<{
   };
 
   const keyIsValid = (key: string | undefined) => {
-    const pattern = /^sk-[a-zA-Z0-9]{48}$/;
+    const pattern = /^(sk-[a-zA-Z0-9]{48}|[a-fA-F0-9]{32})$/;
     return key && pattern.test(key);
   };
 
