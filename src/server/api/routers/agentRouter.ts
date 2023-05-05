1ee46546ecfce866b804eff0452105177e2646cc
Dogtiti <499960698@qq.com>
2023-05-05 22:33:08 +0800

    fix: taskId missing

--- src/server/api/routers/agentRouter.ts
+++ src/server/api/routers/agentRouter.ts
@@ -26,6 +26,7 @@ export const agentRouter = createTRPCRouter({
         return prisma.agentTask.create({
           data: {
             agentId: agent.id,
+            taskId: e.taskId,
             parentTaskId: e.parentTaskId,
             type: e.type,
             ...(e.type === MESSAGE_TYPE_TASK && { status: e.status }),
