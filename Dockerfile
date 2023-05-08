dda24372ed3f7ef7101062a4f9cdf02ab4eb4bf4
Dogtiti <499960698@qq.com>
2023-05-08 21:04:07 +0800

    feat: docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
-# Use the official Node.js image as the base image
+# Use the official Node.js image
 FROM node:19-alpine
 
-ARG NODE_ENV
 
-ENV NODE_ENV=$NODE_ENV
+RUN apk update && apk add --no-cache openssl
+
+
+ARG DATABASE_URL
+ENV DATABASE_URL=$DATABASE_URL
+ARG NEXTAUTH_SECRET=$(openssl rand -base64 32)
+ENV NEXTAUTH_SECRET=$NEXTAUTH_SECRET
+ARG NEXTAUTH_URL
+ENV NEXTAUTH_URL=$NEXTAUTH_URL
+ENV NODE_ENV=production
+ARG SKIP_ENV_VALIDATION
+ENV SKIP_ENV_VALIDATION=$SKIP_ENV_VALIDATION
 
 # Set the working directory
 WORKDIR /app
@@ -11,17 +21,25 @@ WORKDIR /app
 # Copy package.json and package-lock.json to the working directory
 COPY package*.json ./
 
-# Install dependencies
-RUN npm ci
-
 # Copy the rest of the application code
 COPY . .
+
+# Expose the port the app will run on
+EXPOSE 3000
+
+# Prevent Husky errors by disabling the `prepare` script
+RUN npm pkg set scripts.prepare="exit 0"
+
+# set npm registry
+RUN npm config set registry 'https://registry.npmmirror.com/'
+
+# Install dependencies
+RUN npm ci
 
 # Build the Next.js app
 RUN npm run build
 
-# Expose the port the app will run on
-EXPOSE 3000
+
 
 ENTRYPOINT ["sh", "entrypoint.sh"]
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
 // @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
@@ -6,41 +5,17 @@ import { withSentryConfig } from "@sentry/nextjs";
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
-import nextI18NextConfig from './next-i18next.config.js'
+import nextI18NextConfig from "./next-i18next.config.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n:nextI18NextConfig.i18n,
+  i18n: nextI18NextConfig.i18n,
   webpack: function (config, options) {
     config.experiments = { asyncWebAssembly: true, layers: true };
     return config;
-  }
+  },
 };
-export default withSentryConfig(config, {
-  // For all available options, see https://github.com/getsentry/sentry-webpack-plugin#options
-
-  // Suppresses source map uploading logs during build
-  silent: true,
-  org: "reworkd",
-  project: "autogpt",
-  }, {
-  // For all available options, see  https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: false,
-
-  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-  environment: process.env.NEXT_PUBLIC_VERCEL_ENV,
-});
+export default config;
